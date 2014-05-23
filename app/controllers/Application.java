9738a1ba484e0a69290fb732f0171ee2554d3b5a
aboullaite <aboullaite.mohammed@gmail.com>
2014-05-23 20:59:59 +0100

    fix some jdbc conncetion issues

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,8 @@ package controllers;
 
 
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -19,18 +21,18 @@ public class Application extends Controller {
         return ok("ok");
     }
     
-    public static Result saveVote(Long idSession, Long idVoter, Integer vote, String time) {
+    public static Result saveVote(String idSession, String idVoter, String vote, String time) {
     	try {
     	DataSource ds= DB.getDataSource();
     	Connection conn=DB.getConnection();
     	String INSERT_SQL = "INSERT INTO votes " + "(idSession, idVoter, vote, time) "
-                + " VALUES " + "("+idSession+", "+idVoter +", "+vote+", '"+time+"')";
+                + " VALUES " + "('"+idSession+"', '"+idVoter +"', '"+vote+"', '"+URLDecoder.decode(time, "UTF-8")+"')";
     	
 			Statement stat=conn.createStatement();
 			stat.execute(INSERT_SQL);
 			conn.close();
 			
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			
 			return ok("error when inserting");
